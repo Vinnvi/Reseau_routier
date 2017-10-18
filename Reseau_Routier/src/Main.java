73f2107ae125e47a1012d735be284e5515afc209
FloLP <florianlepallec@gmail.com>
2017-10-18 10:29:27 +0200

    timer

--- Reseau_Routier/src/Main.java
+++ Reseau_Routier/src/Main.java
@@ -1,4 +1,23 @@
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Main {
-	
+public class Main 
+{
+	Timer t;
+	class Intervalle extends TimerTask 
+	{
+		int nbRepetitions = 1;
+		public Intervalle()
+		{	
+		}
+        @Override
+		public void run() 
+		{
+			while(nbRepetitions > 0)
+			{	
+			}
+			System.out.println("Terminé!");
+			t.cancel();
+		}
+    }
 }
